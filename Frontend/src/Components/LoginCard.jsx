6dfc76cd1817547ed2e2440b425a376660f4f595
FallenCap <fallencap18@gmail.com>
2023-09-01 18:55:52 +0530

    Changes in login card

--- Frontend/src/Components/LoginCard.jsx
+++ Frontend/src/Components/LoginCard.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import animationData1 from '../assets/animation_llufw913.json';
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData1,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
 
 const LoginCard = () => {
   return (
     <React.Fragment>
-      <div className="h-[90vh] w-[90wh] bg-white rounded-2xl shadow-lg flex flex-col justify-center items-center">
-        <h1 className="text-7xl font-bold pb-8 text-slate-600">Login</h1>
-        <form className="flex flex-col">
+      <div className="h-[60vh] w-[90vw] xl:h-[90vh] bg-white rounded-2xl shadow-lg flex flex-col justify-center items-center">
+        <h1 className="text-6xl md:text-7xl font-bold pb-8 text-slate-600 z-10">
+          Login
+        </h1>
+        <form className="flex flex-col z-10">
           <div className="flex flex-col pb-8 px-24">
             <label className="font-semibold text-slate-600 pb-2">Email</label>
             <input
               type="email"
               placeholder="Enter your email address"
-              className="w-96 px-4 py-2 outline-none bg-transparent border-2 border-slate-600 rounded-lg"
+              className="w-80 md:w-96 px-4 py-2 outline-none bg-transparent border-2 border-slate-600 rounded-lg"
             />
           </div>
           <div className="flex flex-col pb-8 px-24">
@@ -21,7 +34,7 @@ const LoginCard = () => {
             <input
               type="password"
               placeholder="Enter your assword"
-              className="w-96 px-4 py-2 outline-none bg-transparent border-2 border-slate-600 rounded-lg"
+              className="w-80 md:w-96 px-4 py-2 outline-none bg-transparent border-2 border-slate-600 rounded-lg"
             />
           </div>
           <div className="flex justify-center items-center pb-8">
@@ -35,12 +48,15 @@ const LoginCard = () => {
           <div className="flex justify-center">
             <button
               type="submit"
-              className="transition-[bg-gradient] ease-in-out-out delay-300 bg-gradient-to-t from-candy1 to-candy2 hover:bg-gradient-to-b duration-500 w-1/2 px-4 py-3 text-white text-xl font-bold rounded-xl"
+              className="transition-[bg-gradient] ease-in-out-out delay-300 bg-gradient-to-t from-candy1 to-candy2 hover:bg-gradient-to-b duration-500 w-1/3 md:w-1/2 px-4 py-3 text-white text-base md:text-xl font-bold rounded-xl"
             >
               Login
             </button>
           </div>
         </form>
+      </div>
+      <div className="absolute left-0 right-0">
+        <Lottie options={defaultOptions} className="h-1/3 w-1/3" />
       </div>
     </React.Fragment>
   );
